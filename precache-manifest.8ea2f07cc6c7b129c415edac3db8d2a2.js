520ab637c8f404048c577bd970c78f16175e188c
Dumenco <tt-3484@ukr.net>
2020-06-09 22:35:37 +0300

    Updates

---
+++ precache-manifest.8ea2f07cc6c7b129c415edac3db8d2a2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52498c49a56f24d4d5440e08baaeba12",
+    "url": "/goit-react-hw-03-phonebook/./index.html"
+  },
+  {
+    "revision": "ae537f639bd92afe8f11",
+    "url": "/goit-react-hw-03-phonebook/./static/css/main.9867a545.chunk.css"
+  },
+  {
+    "revision": "2407070b5f7f2eb6c58d",
+    "url": "/goit-react-hw-03-phonebook/./static/js/2.f98a522a.chunk.js"
+  },
+  {
+    "revision": "ae537f639bd92afe8f11",
+    "url": "/goit-react-hw-03-phonebook/./static/js/main.221e9d04.chunk.js"
+  },
+  {
+    "revision": "0df61ce22399c8bd320d",
+    "url": "/goit-react-hw-03-phonebook/./static/js/runtime-main.8766f25e.js"
+  }
+]);
